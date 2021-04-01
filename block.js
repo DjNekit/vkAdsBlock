6972865f1eb628273d36e53e827903c8b45d52fe
nikita <nikitalipin97@gmail.com>
2021-04-01 14:03:48 +0300

    v2

--- block.js
+++ block.js
@@ -1,17 +1,22 @@
-const blockAds = obj => {
-	Object.values(obj).forEach(block => {
-		if (block && block.style.display !== 'none') block.style.display = 'none'
-	})
+const adsBlocks = [
+    '#ads_left',
+    '.apps_feedRightAppsBlock',
+    '#feed_blog_reminder',
+    '.ShortVideoStub'
+]
+
+const hideAds = () => {
+    const style = document.createElement('style')
+
+    adsBlocks.forEach(selector => {
+        style.insertAdjacentHTML('beforeend', `
+            ${selector} {
+                display: none!important;
+            }
+        `)
+    })
+
+    document.head.insertAdjacentElement('beforeend', style)
 }
 
-const adsClear = setInterval(() => {
-	const adsFields = {
-		leftBlockWithAds: document.querySelector('#ads_left'),
-		audioBanner: document.querySelector('.CatalogSection'),
-		appsBlock: document.querySelector('.apps_feedRightAppsBlock'),
-		feedBlock: document.querySelector('#feed_blog_reminder'),
-		shortVideoStub: document.querySelector('.ShortVideoStub'),
-	}
-
-	blockAds(adsFields);
-}, 500)
+hideAds()
